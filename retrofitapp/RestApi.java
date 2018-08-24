661ff64f3c9345d78ef8be263179787e064bc09b
L-is-0 <hazelwang0729@gmail.com>
2018-08-24 12:09:19 +0100

    Add files via upload

--- retrofitapp/RestApi.java
+++ retrofitapp/RestApi.java
@@ -4,44 +4,27 @@ import android.location.Location;
 
 import java.util.List;
 
-import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.Path;
-
+import retrofit2.http.Query;
+import retrofit2.http.Url;
 /*
 * define the base_url as the root url of our application
 * retrofit divide the url into two parts, one part is the base url while another part is the api name
 * define the api name and call type, so the api will gives us the content only type of RetroLocation
 * */
+//String BASE_URL = "https://simplifiedcoding.net/demos/";
+
 
 public interface RestApi {
-    //String BASE_URL = "https://simplifiedcoding.net/demos/";
-    public static final String BASE_URL = "https://demo.openmrs.org/openmrs/ws/rest/v1/";
-    public static final String AUTHORIZATION_TOKEN = "authorisation";
+    String BASE_URL = "https://demo.openmrs.org/openmrs/ws/rest/v1/";
 
-    String USERNAME = "admin";
-    String PASSWORD = "Admin123";
-    String LOGIN = "login";
+    @GET("location?tag=Login%20Location")
+    Call<Results<LocationObject>> getLocation(@Query("v") String representation);
 
-
-    //@GET("location")
-    @GET("Location")
-    Call<List<RetroLocation>> getLocation(
-           // @Header("Authentication")String authkey,
-            //@Path("username") String username
-    );
-
-//    @GET("location")
-//    Call<List<RetroLocation>> getLocation();
-
-//    @GET("location?tag=Login%20Location")
-//    Call<List<Location>> getLocations(@Query("v") String representation);
-//
-//    @GET()
-//    Call<List<Location>> getLocations(@Url String url,
-//                                         @Query("tag") String tag,
-//                                         @Query("v") String representation);
-
+    @GET()
+    Call<Results<Location>> getLocation(@Url String url,
+                                         @Query("tag") String tag,
+                                         @Query("v") String representation);
 }
+
